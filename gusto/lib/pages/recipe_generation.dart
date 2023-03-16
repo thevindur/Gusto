044aa88bb59cb7c0c9dff7b4f7d40b5c9867afe6
thevindur <thevindu.2019595@iit.ac.lk>
2023-03-16 23:00:14 +0000

    Creating the grid view to load data from the JSON

--- gusto/lib/pages/recipe_generation.dart
+++ gusto/lib/pages/recipe_generation.dart
@@ -1,15 +1,66 @@
 import 'package:flutter/material.dart';
 
-class RecipeGeneration extends StatelessWidget {
+import '../models/recipe_model.dart';
+
+class RecipeGeneration extends StatefulWidget {
   const RecipeGeneration({Key? key}) : super(key: key);
 
   @override
+  State<RecipeGeneration> createState() => _RecipeGenerationState();
+}
+
+class _RecipeGenerationState extends State<RecipeGeneration> {
+  List<RecipeModel> recipies = [];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFFFE9C8),
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFE9C8),
       body: Center(
-        child: Text('Recipe Search'),
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Text("Gusto"),
+            ),
+            Container(
+              child: GridView(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      mainAxisSpacing: 10.0, maxCrossAxisExtent: 200.0),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  physics: ClampingScrollPhysics(),
+                  children: List.generate(recipies.length, (index) {
+                    return GridTile(
+                        child: RecipeTile(
+                          title: recipies[index].title,
+                          ingredients: recipies[index].ingredients,
+                          instructions: recipies[index].instructions,
+                          image_name: recipies[index].image_name,
+                        ));
+                  })),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
+
+class RecipeTile extends StatefulWidget {
+  final String title, ingredients, instructions, image_name;
+  const RecipeTile({Key? key, required this.title, required this.ingredients, required this.instructions, required this.image_name}) : super(key: key);
+
+  @override
+  State<RecipeTile> createState() => _RecipeTileState();
+}
+
+class _RecipeTileState extends State<RecipeTile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
